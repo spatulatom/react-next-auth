49060b3be945d3ffefae452c0c5383e97e381da0
spatulatom <spatulatom@gmail.com>
2022-12-09 10:19:26 +0000

    useRouter

--- pages/profile.js
+++ pages/profile.js
@@ -1,27 +1,39 @@
 import { getSession } from 'next-auth/react';
 
 import UserProfile from '../components/profile/user-profile';
+import { useSession, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 function ProfilePage() {
-  
-  return <UserProfile />;
-}
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-export async function getServerSideProps(context) {
-  const session = await getSession({ req: context.req });
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/auth',
-        permanent: false,
-      },
-    };
+  console.log('router', router)
+  if (status === 'authenticated') {
+    return <UserProfile/>
   }
-
-  return {
-    props: { session },
-  };
+  if(status==="unauthenticated"){
+    router.replace('/')
+  }
+  
 }
+
+// export async function getServerSideProps(context) {
+//   const session = await getSession({ req: context.req });
+//   console.log('context', context)
+
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: '/',
+//         permanent: false,
+//       },
+//     };
+//   }
+
+//   return {
+//     props: { session },
+//   };
+// }
 
 export default ProfilePage;

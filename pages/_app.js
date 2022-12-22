4f7202464e9b8899feb3e5e8f43fad41692db044
spatulatom <spatulatom@gmail.com>
2022-12-22 10:58:15 +0000

    adding store context api

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,17 @@ import { SessionProvider } from "next-auth/react"
 
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
+import { NotificationContextProvider } from '../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
   return (
+    <NotificationContextProvider>
     <SessionProvider session={pageProps.session}>
       <Layout>
         <Component {...pageProps}/>
       </Layout>
     </SessionProvider>
+    </NotificationContextProvider>
   );
 }
 

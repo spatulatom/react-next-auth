21e2d9f938dc6caf312f0f4f9f11136b2cb13985
spatulatom <spatulatom@gmail.com>
2024-03-08 10:55:47 +0000

    emoticons

--- pages/index.js
+++ pages/index.js
@@ -98,7 +98,7 @@ export default function kkks(props) {
           {/* Main Container */}
           <div className="max-w-lg mx-auto py-24">
             <p className="mb-6 text-lg tracking-widest text-center text-white uppercase">
-              35,000+ Already Joined
+              35,000+ Already Joined ;)
             </p>
             <h2 className="px-3 mb-6 text-3xl font-semibold text-center text-white md:text-4xl"></h2>
           </div>
